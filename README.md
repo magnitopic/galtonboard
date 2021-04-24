ba840f5911dc133d84bc1011690400da6730d165
magnitopic <magnitopic@gmail.com>
2021-04-24 19:22:46 +0200

    New version with matplotlib

---
+++ README.md
@@ -0,0 +1,7 @@
+# Galton Board
+
+A project where we make a galton board in Python.
+
+There are two versions:
+- Just the algorithm. I determines the number of balls in each of the lanes
+- Using the same algorithm as before but representing it in a chart
--- galtonboard-chart.py
+++ galtonboard-chart.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt   
 from random import randint
 
-levels=int(input("How many levels do you want?(min 1) ") or 10)
+levels=int(input("How many levels do you want?(min 1) ") or 12)
 x=np.arange(0,levels+1) 
 if levels>=1:
     lanes=[0]*(levels+1)
@@ -10,9 +10,13 @@ else:
     print("The value of levels can't be lower than 1.")
     exit()
 
+values=[]
 for h in range(0,(levels+1)**2*100):
     stored=0
     for j in range(0,levels):
         stored+=randint(0,1)
-    lanes[stored]+=1
-print(lanes)
+    values.append(stored)
+x = np.random.normal(size=1000)
+print(x)
+plt.hist(values, bins=10)
+plt.show()
