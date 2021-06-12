0a831bdf36ce36cc9ab4717b54de1414d072e338
magnitopic <magnitopic@gmail.com>
2021-06-12 23:44:29 +0200

    New algorithm that has same number of lanes as indicated

--- galtonboard-chart.py
+++ galtonboard-chart.py
@@ -1,23 +1,24 @@
-import numpy as np  
-import matplotlib.pyplot as plt   
+import numpy as np
+import matplotlib.pyplot as plt
 from random import randint
 
-levels=int(input("How many levels do you want?(min 1/ 20 by default) ") or 20)
-x=np.arange(0,levels+1) 
-if levels>=1:
-    lanes=[0]*(levels+1)
+levels = int(input("How many levels do you want?(min 1/ 20 by default) ") or 20)
+
+if levels >= 1:
+    lanes = [0]*(levels)
 else:
     print("The value of levels can't be lower than 1.")
     exit()
 
-for h in range(0,(levels+1)**2*100):
-    stored=0
-    for j in range(0,levels):
-        stored+=randint(0,1)
-    lanes[stored]+=1
+for h in range((levels)**2*100):
+    stored = -1
+    for j in range(levels):
+        stored += randint(0, 1)
+    lanes[stored] += 1
+print((levels)**2*100, "balls were used in totall")
 print(lanes)
 
-X = np.arange(-((len(lanes)/2)-.5),(len(lanes)/2)+.5)
+X = np.arange(-((len(lanes)/2)-.5), (len(lanes)/2)+.5)
 plt.suptitle('Galton Board')
-plt.bar(X + 0.00, lanes, width = 0.25)
+plt.bar(X + 0.00, lanes, width=0.25)
 plt.show()
