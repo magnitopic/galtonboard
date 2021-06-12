c42f61698074da1f7e2ccbce8b7fbb98599ad331
magnitopic <magnitopic@gmail.com>
2021-06-12 20:52:05 +0200

    Galtonboard now shows as chart

--- galtonboard-chart.py
+++ galtonboard-chart.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt   
 from random import randint
 
-levels=int(input("How many levels do you want?(min 1) ") or 12)
+levels=int(input("How many levels do you want?(min 1/ 20 by default) ") or 20)
 x=np.arange(0,levels+1) 
 if levels>=1:
     lanes=[0]*(levels+1)
@@ -10,13 +10,14 @@ else:
     print("The value of levels can't be lower than 1.")
     exit()
 
-values=[]
 for h in range(0,(levels+1)**2*100):
     stored=0
     for j in range(0,levels):
         stored+=randint(0,1)
-    values.append(stored)
-x = np.random.normal(size=1000)
-print(x)
-plt.hist(values, bins=10)
+    lanes[stored]+=1
+print(lanes)
+
+X = np.arange(-((len(lanes)/2)-.5),(len(lanes)/2)+.5)
+plt.suptitle('Galton Board')
+plt.bar(X + 0.00, lanes, width = 0.25)
 plt.show()
